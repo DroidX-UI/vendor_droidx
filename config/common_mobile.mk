111f06a13bebd09ceaf212a1837ccecc6249dfd8
Dyneteve <dyneteve@hentaios.com>
2020-12-13 00:57:11 +0900

    config: Build the textclassifier models
    Change-Id: I9d7d582ddc427998e68e668cc0375958325d8052
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -13,3 +13,14 @@ PRODUCT_PACKAGES += \
 # Media
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     media.recorder.show_manufacturer_and_model=true
+
+# SystemUI plugins
+PRODUCT_PACKAGES += \
+    QuickAccessWallet
+
+# TextClassifier
+PRODUCT_PACKAGES += \
+    libtextclassifier_annotator_en_model \
+    libtextclassifier_annotator_universal_model \
+    libtextclassifier_actions_suggestions_universal_model \
+    libtextclassifier_lang_id_model
