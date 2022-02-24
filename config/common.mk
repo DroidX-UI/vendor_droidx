f649ca61105fa1253c8dac5c6f661c0a91f079e6
jhonboy121 <alfredmathew05@gmail.com>
2022-02-24 11:20:44 +0000

    vendor: introduce app lock [4/4]
    Change-Id: Ie46e8db45b21bce08536437989425955c265f5a4
    Signed-off-by: anupam41 <anupamsamanta41@gmail.com>
    Signed-off-by: hipexscape <abhinav.115260@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -70,6 +70,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/droidx/prebuilt/common/etc/init/init.custom-system_ext.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.custom-system_ext.rc
 
+# App lock permission
+PRODUCT_COPY_FILES += \
+    vendor/droidx/config/permissions/privapp-permissions-settings.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-settings.xml
+
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.sip.voip.xml
