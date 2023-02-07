17e619d6f6cc3da22b0992b795682e8156e28ac7
SamarV-121 <samarvispute121@pm.me>
2023-02-07 12:11:15 +0100

    common: Disable continuous transaction tracing on userdebug builds
    Change-Id: I95657a00e38f1229675682e784b20cca12c119b0
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -302,6 +302,11 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     dalvik.vm.systemuicompilerfilter=speed
 
+ifeq ($(TARGET_BUILD_VARIANT),userdebug)
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    debug.sf.enable_transaction_tracing=false
+endif
+
 # SetupWizard
 PRODUCT_PRODUCT_PROPERTIES += \
     setupwizard.feature.day_night_mode_enabled=true \
