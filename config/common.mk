Evolution-X-AOSP/vendor_evolution

config: Grant android.permission.WRITE_DEVICE_CONFIG to gms
Grant android.permission.WRITE_DEVICE_CONFIG to com.google.android.gms
package.

TEST: run gts -m GtsDeviceConfigTestCases -t
com.google.android.deviceconfig.gts.DeviceConfigApiPermissionTests#testWriteDeviceConfigPermission

Signed-off-by: Haoran.Wang <elven.wang@nxp.com>
Reviewed-on: http://androidsource.ap.freescale.net/project/13666
Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>
Reviewed-by: Tian Yang <yang.tian@nxp.com>

config/common.mk
@@ -107,6 +107,10 @@ PRODUCT_COPY_FILES += \
     vendor/evolution/prebuilt/common/etc/init/init.evolution-x-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.evolution-x-updater.rc \
     vendor/evolution/prebuilt/common/etc/init/smartcharge-init.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/smartcharge-init.rc
 
+# GMS Permissions
+PRODUCT_COPY_FILES += \
+    vendor/evolution/config/permissions/privapp-permissions-gms.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-gms.xml
+
 # LineageHW permission
 PRODUCT_COPY_FILES += \
     vendor/evolution/config/permissions/privapp-permissions-lineagehw.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-lineagehw.xml

DroidX-UI/vendor_droidx

config/common.mk
@@ -57,6 +57,10 @@ endif
 PRODUCT_COPY_FILES += \
     vendor/droidx/prebuilt/common/etc/init/init.droidx-system_ext.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.droidx-system_ext.rc
 
+# GMS Permissions
+PRODUCT_COPY_FILES += \
+    vendor/droidx/config/permissions/privapp-permissions-gms.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-gms.xml
+
 # Enable Android Beam on all targets
 PRODUCT_COPY_FILES += \
     vendor/droidx/config/permissions/android.software.nfc.beam.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.nfc.beam.xml