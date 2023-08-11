41ab3d7cf598890f8531d771499f2c0aa8e3df99
afterallafk <shivamatiet2001@gmail.com>
2023-08-11 18:46:06 +0530

    config: Add missing eng build props for debugging
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -24,9 +24,11 @@ endif
 ifeq ($(TARGET_BUILD_VARIANT),eng)
 # Disable ADB authentication
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=0
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.usb.config=adb
 else
 # Enable ADB authentication
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=1
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.usb.config=none
 
 # Disable extra StrictMode features on all non-eng builds
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.strictmode.disable=true
