9ad11948fbec5711a8c3529e1f7542b58d51c011
NRanjan-17 <nalinishranjan05@gmail.com>
2023-09-10 22:33:11 +0530

    common: add some flags

--- config/common.mk
+++ config/common.mk
@@ -101,6 +101,12 @@ PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 # Disable vendor restrictions
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
+# customization
+TARGET_SUPPORTS_GOOGLE_RECORDER ?= true
+TARGET_INCLUDE_STOCK_ARCORE ?= true
+TARGET_SUPPORTS_CALL_RECORDING ?= true
+TARGET_USE_GOOGLE_TELEPHONY ?= true
+
 ifneq ($(TARGET_DISABLE_EPPE),true)
 # Require all requested packages to exist
 $(call enforce-product-packages-exist-internal,$(wildcard device/*/$(DROIDX_BUILD)/$(TARGET_PRODUCT).mk),product_manifest.xml rild Calendar Launcher3 Launcher3Go Launcher3QuickStep Launcher3QuickStepGo android.hidl.memory@1.0-impl.vendor vndk_apex_snapshot_package)
