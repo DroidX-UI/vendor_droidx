7408eeba7e83cd2b7a54a35c06c7c961c5f4424d
someone5678 <someone5678@users.noreply.github.com>
2024-06-05 12:21:08 +0900

    vendor: config: props: Set default wifi country code
    * To 00
    * Make it overridable
    
    Change-Id: I2025fc927363baf547b36ecd6ebe9c7fbc4d7611

--- config/common.mk
+++ config/common.mk
@@ -202,8 +202,12 @@ PRODUCT_PACKAGES += \
     rsync
 
 # Storage manager
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+PRODUCT_SYSTEM_PROPERTIES += \
     ro.storage_manager.enabled=true
+
+# Default wifi country code
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.boot.wificountrycode?=00
 
 # These packages are excluded from user builds
 PRODUCT_PACKAGES_DEBUG += \
