0525f0e41ab7ffc98f0c66d3a1ce42713fddbf3f
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-07-21 22:01:35 +0200

    config: tablet: Enable freeform and large screen support for all targets
    Change-Id: I8137911f96cf2eb347d8e5012be649ddb01057f8
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -1,7 +1,7 @@
 # Inherit full common DroidX-UI stuff
 $(call inherit-product, vendor/droidx/config/common_mobile_full.mk)
 
-# Inherit full tablet common Lineage stuff
-$(call inherit-product, vendor/droidx/config/full_tablet.mk)
+# Inherit tablet common DroidX-UI stuff
+$(call inherit-product, vendor/droidx/config/tablet.mk)
 
 $(call inherit-product, vendor/droidx/config/wifionly.mk)
--- config/common_mini_tablet.mk
+++ config/common_mini_tablet.mk
@@ -1,4 +1,7 @@
 # Inherit mobile mini common DroidX-UI stuff
 $(call inherit-product, vendor/droidx/config/common_mobile_mini.mk)
 
+# Inherit tablet common DroidX-UI stuff
+$(call inherit-product, vendor/droidx/config/tablet.mk)
+
 $(call inherit-product, vendor/droidx/config/telephony.mk)

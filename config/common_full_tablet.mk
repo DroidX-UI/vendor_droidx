LineageOS/android_vendor_lineage

config: tablet: Enable freeform and large screen support for all targets
Change-Id: I8137911f96cf2eb347d8e5012be649ddb01057f8

config/common_full_tablet.mk
@@ -1,7 +1,7 @@
 # Inherit mobile full common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_mobile_full.mk)
 
-# Inherit full tablet common Lineage stuff
-$(call inherit-product, vendor/lineage/config/full_tablet.mk)
+# Inherit tablet common Lineage stuff
+$(call inherit-product, vendor/lineage/config/tablet.mk)
 
 $(call inherit-product, vendor/lineage/config/telephony.mk)

config/common_full_tablet_wifionly.mk
@@ -1,7 +1,7 @@
 # Inherit mobile full common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_mobile_full.mk)
 
-# Inherit full tablet common Lineage stuff
-$(call inherit-product, vendor/lineage/config/full_tablet.mk)
+# Inherit tablet common Lineage stuff
+$(call inherit-product, vendor/lineage/config/tablet.mk)
 
 $(call inherit-product, vendor/lineage/config/wifionly.mk)

config/common_mini_tablet.mk
@@ -1,4 +1,7 @@
 # Inherit mobile mini common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_mobile_mini.mk)
 
+# Inherit tablet common Lineage stuff
+$(call inherit-product, vendor/lineage/config/tablet.mk)
+
 $(call inherit-product, vendor/lineage/config/telephony.mk)

config/common_mini_tablet_wifionly.mk
@@ -1,4 +1,7 @@
 # Inherit mobile mini common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_mobile_mini.mk)
 
+# Inherit tablet common Lineage stuff
+$(call inherit-product, vendor/lineage/config/tablet.mk)
+
 $(call inherit-product, vendor/lineage/config/wifionly.mk)

DroidX-UI/vendor_droidx

Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

config/common_full_tablet.mk
@@ -1,7 +1,7 @@
 # Inherit full common DroidX-UI stuff
 $(call inherit-product, vendor/droidx/config/common_mobile_full.mk)
 
-# Inherit full tablet common Lineage stuff
-$(call inherit-product, vendor/droidx/config/full_tablet.mk)
+# Inherit tablet common DroidX-UI stuff
+$(call inherit-product, vendor/droidx/config/tablet.mk)
 
 $(call inherit-product, vendor/droidx/config/telephony.mk)

config/common_full_tablet_wifionly.mk
@@ -1,7 +1,7 @@
 # Inherit full common DroidX-UI stuff
 $(call inherit-product, vendor/droidx/config/common_mobile_full.mk)
 
-# Inherit full tablet common Lineage stuff
-$(call inherit-product, vendor/droidx/config/full_tablet.mk)
+# Inherit tablet common DroidX-UI stuff
+$(call inherit-product, vendor/droidx/config/tablet.mk)
 
 $(call inherit-product, vendor/droidx/config/wifionly.mk)

config/common_mini_tablet.mk
@@ -1,4 +1,7 @@
 # Inherit mobile mini common DroidX-UI stuff
 $(call inherit-product, vendor/droidx/config/common_mobile_mini.mk)
 
+# Inherit tablet common DroidX-UI stuff
+$(call inherit-product, vendor/droidx/config/tablet.mk)
+
 $(call inherit-product, vendor/droidx/config/telephony.mk)

config/common_mini_tablet_wifionly.mk
@@ -1,4 +1,7 @@
 # Inherit mobile mini common DroidX-UI stuff
 $(call inherit-product, vendor/droidx/config/common_mobile_mini.mk)
 
+# Inherit tablet common DroidX-UI stuff
+$(call inherit-product, vendor/droidx/config/tablet.mk)
+
 $(call inherit-product, vendor/droidx/config/wifionly.mk)