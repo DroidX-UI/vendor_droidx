4ef7745b80e4350ed54ebdbef87681220d6651c3
hipexscape <abhinav.115260@gmail.com>
2024-08-09 15:47:57 +0000

    aconfig: Enable all of the useful flags
    Co-authored-by: someone5678 <someone5678@users.noreply.github.com>
    Change-Id: I64f2370d18f7d1f03f1b466b4abcbf6323871add

--- release/release_config_map.mk
+++ release/release_config_map.mk
@@ -13,10 +13,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Get the target release version
+TARGET_RELEASE := $(shell grep "BUILD_ID" build/make/core/build_id.mk | tail -1 | cut -d '=' -f 2 | cut -d '.' -f 1 | tr '[:upper:]' '[:lower:]')
+
 # Get the directory for this file, and use that instead of a fixed path.
 local_dir := $(dir $(lastword $(MAKEFILE_LIST)))
 
 # Attach the flag value definitions to the various release configurations.
-$(call declare-release-config, ap2a, $(local_dir)build_config/ap2a.scl)
+$(call declare-release-config, $(TARGET_RELEASE), $(local_dir)build_config/$(TARGET_RELEASE).scl)
 
 local_dir :=
