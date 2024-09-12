0f9a7d272953df4fcc73731d6b62ad57f4811b55
Michael Bestas <mkbestas@gmail.com>
2024-09-12 03:51:01 +0300

    build: Move version to PRODUCT_SYSTEM_PROPERTIES
    ADDITIONAL_SYSTEM_PROPERTIES is being deprecated.
    No functional changes.
    
    Change-Id: I01fd45edcc6c700540d3b7352fc466e65677a44e
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -3,8 +3,3 @@ ifneq ($(BUILD_FINGERPRINT),)
 ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
 endif
-
-# DroidX-UI System Version
-ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.modversion=$(DROIDX_VERSION)
-    
