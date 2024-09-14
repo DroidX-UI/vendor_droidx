d4d6055109a713634c09e9ca02f164027d6ffa7e
ZirgomHaidar <zirgomhaidar17@gmail.com>
2024-09-14 14:51:37 +0530

    Uprev to v2.3
    Android 14.0.0 Release 67 (AP2A.240905.003)

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
 # Shebang is intentionally missing - do not run as a script
 
 # Override host metadata to make builds more reproducible and avoid leaking info
-export BUILD_USERNAME=eris
+export BUILD_USERNAME=mars
 export BUILD_HOSTNAME=droidx
 
 # ABI compatibility checks fail for several reasons:
