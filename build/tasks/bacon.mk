LineageOS/android_vendor_lineage

bacon: Make it possible to let other build targets depend on lineage OTA package
Change-Id: I04ed91b62beed9728c354ea73f7c0baa72213808

build/tasks/bacon.mk
@@ -20,8 +20,10 @@ LINEAGE_TARGET_PACKAGE := $(PRODUCT_OUT)/lineage-$(LINEAGE_VERSION).zip
 
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 
-.PHONY: bacon
-bacon: $(DEFAULT_GOAL) $(INTERNAL_OTA_PACKAGE_TARGET)
+$(LINEAGE_TARGET_PACKAGE): $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(LINEAGE_TARGET_PACKAGE)
 	$(hide) $(SHA256) $(LINEAGE_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(LINEAGE_TARGET_PACKAGE).sha256sum
 	@echo "Package Complete: $(LINEAGE_TARGET_PACKAGE)" >&2
+
+.PHONY: bacon
+bacon: $(LINEAGE_TARGET_PACKAGE) $(DEFAULT_GOAL)

DroidX-UI/vendor_droidx

Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

build/tasks/bacon.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 # -----------------------------------------------------------------
-# Lineage OTA update package
+# DroidX-UI OTA update package
 ifeq ($(DROIDX_ZIP_TYPE), Gapps)
 DROIDX_TARGET_PACKAGE := $(PRODUCT_OUT)/droidx-$(DROIDX_VERSION)-Gapps.zip
 else
@@ -25,8 +25,7 @@ DROIDX_OTA_PACKAGE := droidx-$(DROIDX_VERSION)-$(DROIDX_ZIP_TYPE).zip
 
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 
-.PHONY: bacon
-bacon: $(DEFAULT_GOAL) $(INTERNAL_OTA_PACKAGE_TARGET)
+$(DROIDX_TARGET_PACKAGE): $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(DROIDX_TARGET_PACKAGE)
 	$(hide) $(SHA256) $(DROIDX_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(DROIDX_TARGET_PACKAGE).sha256sum
 	$(hide) ./vendor/droidx/build/tools/generate_json.sh $(TARGET_DEVICE) $(PRODUCT_OUT) $(DROIDX_OTA_PACKAGE)
@@ -37,3 +36,6 @@ bacon: $(DEFAULT_GOAL) $(INTERNAL_OTA_PACKAGE_TARGET)
 	echo -e ${CL_BLD}${CL_GRN}"TimeStamp:"${CL_RED}" `cat $(PRODUCT_OUT)/system/build.prop | grep ro.system.build.date.utc | cut -d'=' -f2 | awk '{print $$1}' `"${CL_RST}
 	echo -e ${CL_BLD}${CL_GRN}"Integer Value:"${CL_RED}" `wc -c $(DROIDX_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
 	echo -e ${CL_BLD}${CL_RED}"================================================================================"${CL_RED}
+
+.PHONY: bacon
+bacon: $(DROIDX_TARGET_PACKAGE) $(DEFAULT_GOAL)