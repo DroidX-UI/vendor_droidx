51dd0a805f98cbc1d6ee0f953e618696e41a2e72
LuK1337 <priv.luk@gmail.com>
2024-11-04 20:24:31 +0100

    config: Add text classifier files to allowed product artifacts
    Change-Id: Ia568e8ad8aec14784f1649ec4e14b961a3c17f91
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -24,3 +24,9 @@ PRODUCT_PACKAGES += \
     libtextclassifier_annotator_universal_model \
     libtextclassifier_actions_suggestions_universal_model \
     libtextclassifier_lang_id_model
+
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/etc/textclassifier/actions_suggestions.universal.model \
+    system/etc/textclassifier/lang_id.model \
+    system/etc/textclassifier/textclassifier.en.model \
+    system/etc/textclassifier/textclassifier.universal.model
