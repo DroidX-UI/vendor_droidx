7868df65496155afcd6ae2b879dd520f3c45953f
Michael Bestas <mkbestas@lineageos.org>
2025-03-26 19:25:20 +0200

    soong: move EGL display array config to select
    Change-Id: Ia03dd6b49637ed3b7432df26824fcbf01d92106b
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -51,7 +51,6 @@ SOONG_CONFIG_droidxGlobalVars += \
     target_trust_usb_control_path \
     target_trust_usb_control_enable \
     target_trust_usb_control_disable \
-    uses_egl_display_array \
     uses_oplus_camera \
     uses_nothing_camera \
     include_miui_camera \
@@ -69,7 +68,6 @@ SOONG_CONFIG_droidxGlobalVars_gralloc_handle_has_custom_content_md_reserved_size
 SOONG_CONFIG_droidxGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
 SOONG_CONFIG_droidxGlobalVars_gralloc_handle_has_ubwcp_format := $(TARGET_GRALLOC_HANDLE_HAS_UBWCP_FORMAT)
 SOONG_CONFIG_droidxGlobalVars_target_alternative_futex_waiters := $(TARGET_ALTERNATIVE_FUTEX_WAITERS)
-SOONG_CONFIG_droidxGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
 SOONG_CONFIG_droidxGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_droidxGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 SOONG_CONFIG_droidxGlobalVars_uses_miui_camera := $(TARGET_USES_MIUI_CAMERA)
