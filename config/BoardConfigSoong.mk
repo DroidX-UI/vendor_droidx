b09fb4edab50cd518d35f1543474ac67fae6c540
Michael Bestas <mkbestas@lineageos.org>
2025-03-26 19:00:38 +0200

    soong: move QTI surfaceflinger config to select
    Change-Id: If98ed6feb18a5dea66857e507c5e4a369baa5a8b
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -66,13 +66,6 @@ SOONG_CONFIG_NAMESPACES += droidxNvidiaVars
 SOONG_CONFIG_droidxNvidiaVars += \
     uses_nvidia_enhancements
 
-SOONG_CONFIG_NAMESPACES += droidxQcomVars
-
-# Only create display_headers_namespace var if dealing with UM platforms to avoid breaking build for all other platforms
-ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
-SOONG_CONFIG_droidxQcomVars += \
-    qcom_display_headers_namespace
-endif
 
 # Soong bool variables
 SOONG_CONFIG_droidxGlobalVars_camera_override_format_from_reserved := $(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED)
@@ -131,8 +124,3 @@ SOONG_CONFIG_droidxGlobalVars_target_surfaceflinger_udfps_lib := $(TARGET_SURFAC
 SOONG_CONFIG_droidxGlobalVars_target_trust_usb_control_path := $(TARGET_TRUST_USB_CONTROL_PATH)
 SOONG_CONFIG_droidxGlobalVars_target_trust_usb_control_enable := $(TARGET_TRUST_USB_CONTROL_ENABLE)
 SOONG_CONFIG_droidxGlobalVars_target_trust_usb_control_disable := $(TARGET_TRUST_USB_CONTROL_DISABLE)
-ifneq ($(filter $(QSSI_SUPPORTED_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
-SOONG_CONFIG_droidxQcomVars_qcom_display_headers_namespace := vendor/qcom/opensource/commonsys-intf/display
-else
-SOONG_CONFIG_droidxQcomVars_qcom_display_headers_namespace := $(QCOM_SOONG_NAMESPACE)/display
-endif
