3a7a124d913b4f8389c83079bf0417794a5d5828
Michael Bestas <mkbestas@lineageos.org>
2025-03-26 19:01:54 +0200

    soong: move nvidia enhancements config to select
    Original flag is still used by device specific makefile guards.
    
    Change-Id: I528ee2a2a25680994561e4c3fc377f7b57106a89
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -62,10 +62,6 @@ SOONG_CONFIG_droidxGlobalVars += \
     target_health_charging_control_charging_path
 endif
 
-SOONG_CONFIG_NAMESPACES += droidxNvidiaVars
-SOONG_CONFIG_droidxNvidiaVars += \
-    uses_nvidia_enhancements
-
 
 # Soong bool variables
 SOONG_CONFIG_droidxGlobalVars_camera_override_format_from_reserved := $(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED)
@@ -74,7 +70,6 @@ SOONG_CONFIG_droidxGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALL
 SOONG_CONFIG_droidxGlobalVars_gralloc_handle_has_ubwcp_format := $(TARGET_GRALLOC_HANDLE_HAS_UBWCP_FORMAT)
 SOONG_CONFIG_droidxGlobalVars_target_alternative_futex_waiters := $(TARGET_ALTERNATIVE_FUTEX_WAITERS)
 SOONG_CONFIG_droidxGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
-SOONG_CONFIG_droidxNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_droidxGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_droidxGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 SOONG_CONFIG_droidxGlobalVars_uses_miui_camera := $(TARGET_USES_MIUI_CAMERA)
